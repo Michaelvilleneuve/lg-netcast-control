c4f2b831e39cbe91c9a17ca12fbe7824c6f9ccfc
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-05-01 19:16:35 +0200

    Add automatic input change

--- services/controls.js
+++ services/controls.js
@@ -1,10 +1,26 @@
 import API from '../LgApi.js';
+import controlList from '../controls.json';
 
 const controls = {
   handle(req, res) {
     API.command(req.params.control)
       .then(apiStatus => res.sendStatus(apiStatus))
       .catch(err => res.json(err));
+  },
+
+  changeInput(req, res) {
+    API.command('input');
+    setTimeout(() => {
+      API.command('input');
+    }, 1500);
+    setTimeout(() => {
+      API.command('ok');
+    }, 3000);
+    res.sendStatus(200);
+  },
+
+  listCommands(req, res) {
+    res.json(controlList);
   }
 };
 
