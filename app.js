a391a7f84c262f0c97ade7c314acb0418e7da1a5
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-05-01 19:15:58 +0200

    Add input list and custom command

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import API from './LgApi';
 API.pair();
 const app = express();
 
+app.post('/command/change-input', (req, res) => Control.changeInput(req, res));
 app.post('/command/:control', (req, res) => Control.handle(req, res));
+app.get('/list', (req, res) => Control.listCommands(req, res));
 
 app.listen(1234, () => console.log('Listening on port 1234'));
