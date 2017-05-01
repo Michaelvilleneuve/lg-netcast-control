04b722a224f96b55ff8f76a0c2af9f2c8e120328
Michaël Villeneuve <contact@michaelvilleneuve.fr>
2017-05-01 14:20:06 +0200

    Working LG API

--- LgApi.js
+++ LgApi.js
@@ -1,5 +1,6 @@
 import fetch from 'node-fetch';
 import controls from './controls.json';
+/* eslint max-len: 0 */
 
 const API = {
   tv: 'http://192.168.1.10:8080/udap/api/',
@@ -9,7 +10,6 @@ const API = {
   },
 
   command(name) {
-    // eslint-disable-next-line max-len
     const body = `<?xml version="1.0" encoding="utf-8"?><envelope><api type="command"><name>HandleKeyInput</name><value>${controls[name]}</value></api></envelope>`;
     return fetch(`${this.tv}command`, {
       method: 'POST',
@@ -18,11 +18,30 @@ const API = {
     })
     .then((res) => {
       if (res.status === 401) return API.authenticate(this.command.bind(this, name));
+      return res.status;
+    });
+  },
+
+  pair() {
+    const body = '<?xml version="1.0" encoding="utf-8"?> <envelope> <api type="pairing"> <name>showKey</name> </api> </envelope>';
+    return fetch(`${this.tv}pairing`, {
+      method: 'POST',
+      headers: this.headers,
+      body
+    })
+    .then(() => API.finishPairing());
+  },
+
+  finishPairing() {
+    const body = '<?xml version="1.0" encoding="utf-8"?> <envelope> <api type="pairing"> <name>hello</name> <value>272905</value> <port>8080</port> </api> </envelope>';
+    return fetch(`${this.tv}pairing`, {
+      method: 'POST',
+      headers: this.headers,
+      body
     });
   },
 
   authenticate(callback) {
-    // eslint-disable-next-line max-len
     const body = '<?xml version="1.0" encoding="utf-8"?> <envelope> <api type="pairing"> <name>hello</name> <value>272905</value> <port>8080</port> </api> </envelope>';
 
     return fetch(`${this.tv}command`, {
@@ -31,7 +50,9 @@ const API = {
       body
     })
     .then((res) => {
-      console.log(res);
+      if (res.status === 200) {
+        return callback();
+      }
     });
   }
 };
